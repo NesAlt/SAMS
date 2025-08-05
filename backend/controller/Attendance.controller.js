1747614c84d60194c662c8433f31f5ad4dcbc8d4
Nes <nesmain786@gmail.com>
2025-08-05 18:03:58 +0530

    db and server initialized

---
+++ backend/controller/Attendance.controller.js
@@ -0,0 +1,29 @@
+const Attendance = require('../models/Attendance');
+
+const markAttendance = async(req,res)=>{
+
+};
+
+const getAllAttendance = async(req,res)=>{
+
+};
+
+const getAttendanceByStudent = async(req,res)=>{
+
+};
+
+const updateAttendance = async(req,res)=>{
+
+};
+
+const deleteAttendance = async(req,res)=>{
+
+};
+
+module.exports={
+  markAttendance,
+  getAllAttendance,
+  getAttendanceByStudent,
+  updateAttendance,
+  deleteAttendance
+};
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors =require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+if (!process.env.PORT || !process.env.MONGO_URI) {
+  console.error('Missing PORT or MONGO_URI in .env');
+  process.exit(1);
+}
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/attendance', require('./routes'));
+
+const PORT =process.env.PORT
+connectDB().then(()=>{
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
